a0aa86dcbe1b8260667f8aee0713f110a4753340
MatheusCBonafe <warpmatheus@gmail.com>
2021-06-04 15:44:17 -0300

    first commit

--- app/src/main/java/com/example/videoapplication/video/VideoViewModel.kt
+++ app/src/main/java/com/example/videoapplication/video/VideoViewModel.kt
@@ -1,9 +1,11 @@
-package com.example.videoapplication.favorites
+package com.example.videoapplication.video;
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class FavoritesViewModel : ViewModel() {
+class VideoViewModel : ViewModel() {
+
     private val _text = MutableLiveData<String>().apply {
+
     }
 }
